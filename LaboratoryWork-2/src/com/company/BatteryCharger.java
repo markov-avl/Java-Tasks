c97165b69ce3a0c1bdb0602e7cd35874b9043ee0
markov-avl <andrewshasucks@gmail.com>
2021-10-19 11:31:14 +1000

    LW-2 started

---
+++ LaboratoryWork-2/src/com/company/BatteryCharger.java
@@ -0,0 +1,26 @@
+package com.company;
+
+// 2009 - 3 - a
+
+public class BatteryCharger {
+    private final int[] rateTable = {
+            50,  60,  160, 60,  80, 100, 100, 120,
+            150, 150, 150, 200, 40, 240, 220, 220,
+            200, 200, 180, 180, 140, 100, 80, 60
+    };
+
+    /** Determines the total cost to charge the battery starting at the beginning of startHour.
+     * @param startHour the hour at which the charge period begins
+     * Precondition: 0 ≤ startHour ≤ 23
+     * @param chargeTime the number of hours the battery needs to be charged
+     * Precondition: chargeTime > 0
+     * @return the total cost to charge the battery if preconditions are performed else -1
+     */
+    private int getChargingCost(int startHour, int chargeTime) {
+        if (startHour < 0 || startHour > 23 || chargeTime <= 0) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
