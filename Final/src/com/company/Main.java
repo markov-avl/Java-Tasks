b8bf596d56138d41a73c172cfd93456f91e22744
markov-avl <andrewshasucks@gmail.com>
2022-01-11 03:39:36 +1000

    Final

--- Final/src/com/company/Main.java
+++ Final/src/com/company/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Options options = Main.getOptions();
         Watermark watermark = new Watermark();
-
         try {
             CommandLine cmd = new DefaultParser().parse(options, args);
             String text = cmd.getOptionValue("text");
@@ -21,20 +20,23 @@ public class Main {
             new HelpFormatter().printHelp("watermark", options);
             System.exit(1);
         }
+//        watermark.addWatermark("shit", "https://picsum.photos/200/300", "test-out.png");
     }
 
     public static Options getOptions() {
         Options options = new Options();
-        Option text = new Option("t", "text", true, "watermark text");
+        Option text = new Option("t", "text", true,
+                "watermark text");
         Option source = new Option("s", "source", true,
                 "source image path or http/https image link");
-        Option output = new Option("o", "output", true, "watermarked image path");
+        Option output = new Option("o", "output", true,
+                "watermarked image path");
         Option color = new Option("c", "color", true,
                 "color of watermark: " + String.join("|", Colors.all.keySet()));
         Option position = new Option("p", "position", true,
                 "position of watermark: " + String.join("|", Positions.all.keySet()));
         Option size = new Option(null, "size", true,
-                "size of watermark: from 1% to 100%");
+                "size of watermark: from 1% to 100% of source image width");
         Option opacity = new Option(null, "opacity", true,
                 "opacity of watermark: from 1% to 100%");
 
--- Final/src/com/company/Watermark.java
+++ Final/src/com/company/Watermark.java
@@ -1,54 +1,105 @@
 package com.company;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Pattern;
 
 public class Watermark {
     public static final String defaultColor = "white";
     public static final String defaultPosition = "center";
     public static final int defaultSize = 10;
     public static final int defaultOpacity = 10;
+    public static final String fontName = "Arial";
+    public static final int fontStyle = Font.BOLD;
 
     Color color = Colors.all.get(defaultColor);
     int position = Positions.all.get(defaultPosition);
     int opacity = defaultOpacity;
     int size = defaultSize;
 
-    public void addWatermark(String text, String input, String output) {
+    public void addWatermark(String text, String source, String output) {
         try {
-            File sourceImageFile = new File(input);
-            File destImageFile = new File(output);
+            BufferedImage sourceImage = getSourceImage(source);
+            String outputImageName = getOutputImageName(source, output);
+            File outputImage = new File(outputImageName);
 
-            BufferedImage sourceImage = ImageIO.read(sourceImageFile);
-            Graphics2D g2d = (Graphics2D) sourceImage.getGraphics();
+            Graphics2D graphics2D = (Graphics2D) sourceImage.getGraphics();
+            graphics2D.setColor(color);
+            graphics2D.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity / 100));
+            graphics2D.setFont(new Font(fontName, fontStyle, getSize(text, sourceImage)));
 
-            // initializes necessary graphic properties
-            AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity / 100);
-            g2d.setComposite(alphaChannel);
-            g2d.setColor(Color.WHITE);
-            g2d.setFont(new Font("Arial", Font.BOLD, 64));
-            FontMetrics fontMetrics = g2d.getFontMetrics();
-            Rectangle2D rect = fontMetrics.getStringBounds(text, g2d);
+            Rectangle2D textRectangle = graphics2D.getFontMetrics().getStringBounds(text, graphics2D);
+            graphics2D.drawString(text, getX(textRectangle, sourceImage), getY(textRectangle, sourceImage));
 
-            // calculates the coordinate where the String is painted
-            int centerX = (sourceImage.getWidth() - (int) rect.getWidth()) / 2;
-            int centerY = sourceImage.getHeight() / 2;
+            ImageIO.write(sourceImage, "png", outputImage);
+            graphics2D.dispose();
 
-            // paints the textual watermark
-            g2d.drawString(text, centerX, centerY);
-
-            ImageIO.write(sourceImage, "png", destImageFile);
-            g2d.dispose();
-
-            System.out.println("The text watermark is added to the image.");
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("The text watermark is added to the image");
+            System.out.println("The output image saved in " + outputImageName);
+        } catch (IOException ignored) {
+            System.out.println("ERROR: Cannot get source image!");
         }
+    }
+
+    private BufferedImage getSourceImage(String source) throws IOException, IIOException {
+        File image = new File(source);
+        if (image.exists() && image.isFile()) {
+            return ImageIO.read(new File(source));
+        }
+        if (Pattern.matches("^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$", source)) {
+            return ImageIO.read(new URL(source));
+        }
+        throw new FileNotFoundException();
+    }
+
+    private String getOutputImageName(String source, String output) {
+        String endOfName = "-watermarked.png";
+        if (output != null) {
+            if (Pattern.matches("^[-_. A-Za-z0-9]+$", output)) {
+                source = output;
+                endOfName = ".png";
+            } else {
+                System.out.println("Invalid output file name, " +
+                        "so the output file name will be taken from source file name");
+            }
+        }
+        String fileName = new File(source).getName();
+        return (fileName.contains(".") ? fileName.substring(0, fileName.lastIndexOf('.')) : fileName) + endOfName;
+    }
+
+    private int getSize(String text, BufferedImage image) {
+        Graphics2D graphics2D = (Graphics2D) image.getGraphics();
+        int width = image.getWidth() / 100 * size;
+        int s = 0;
+        while (s == 0 || graphics2D.getFontMetrics().getStringBounds(text, graphics2D).getWidth() < width) {
+            graphics2D.setFont(new Font(fontName, fontStyle, ++s));
+        }
+        return s;
+    }
+
+    private int getX(Rectangle2D rectangle2D, BufferedImage image) {
+        if (position == Positions.LEFT_TOP || position == Positions.LEFT_BOTTOM) {
+            return 0;
+        } else if (position == Positions.RIGHT_TOP || position == Positions.RIGHT_BOTTOM) {
+            return image.getWidth() - (int) rectangle2D.getWidth();
+        }
+        return (image.getWidth() - (int) rectangle2D.getWidth()) / 2;
+    }
+
+    private int getY(Rectangle2D rectangle2D, BufferedImage image) {
+        if (position == Positions.LEFT_TOP || position == Positions.RIGHT_TOP) {
+            return (int) rectangle2D.getHeight() / 3 * 2;
+        } else if (position == Positions.LEFT_BOTTOM || position == Positions.RIGHT_BOTTOM) {
+            return image.getHeight() - (int) (rectangle2D.getHeight() / 4);
+        }
+        return (image.getHeight() + (int) rectangle2D.getHeight()) / 2;
     }
 
     public void setColor(String value) {
